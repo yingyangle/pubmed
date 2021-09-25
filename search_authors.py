6a974f2da281510396144842dfdb233c4d8f882d
yingyangle <cyang1999@gmail.com>
2021-09-25 00:32:50 -0400

    finished getting articles by relevant authors

--- search_authors.py
+++ search_authors.py
@@ -9,6 +9,9 @@ from datetime import date, datetime
 from pymed import PubMed
 pubmed = PubMed(tool="PubMedSearcher", email="cyang1999@gmail.com")
 
+start = datetime.now()
+print('start', start)
+
 OUTPUT_FOLDER = 'data/relevant_authors_articles'
 
 # read in authors to search
@@ -18,31 +21,38 @@ print('\n# total relevant authors:', len(authors_to_search))
 
 # read in authors already searched
 try:
-	with open('{OUTPUT_FOLDER}/authors_already_searched.json', 'r') as ein:
+	with open(f'{OUTPUT_FOLDER}/authors_already_searched.json', 'r') as ein:
 		authors_already_searched = json.load(ein)[:-3] # re-search last 3 authors
 	print('# authors already searched:', len(authors_already_searched))
 except: authors_already_searched = []
 
 # read in articles already saved
 try:
-	with open('{OUTPUT_FOLDER}/articles_already_searched.json', 'r') as ein:
+	with open(f'{OUTPUT_FOLDER}/articles_already_saved.json', 'r') as ein:
 		articles_already_saved = json.load(ein)
 	print('# articles already saved:', len(articles_already_saved))
 except: articles_already_saved = []
 
 # read in failed authors
 try:
-	with open('{OUTPUT_FOLDER}/articles_already_searched.json', 'r') as ein:
+	with open(f'{OUTPUT_FOLDER}/failed_authors.json', 'r') as ein:
 		failed_authors = json.load(ein)
 	print('# failed authors so far:', len(failed_authors))
 except: failed_authors = []
 
+# remove authors already searched
+authors_to_search = {k:v for k,v in authors_to_search.items() if k not in authors_already_searched}
+print('# authors to search:', len(authors_to_search))
 print('\n#####################\n')
+
 
 # save articles retrieved for a single author
 def save_results(author_fullname, author_articles):
+	global authors_already_searched, articles_already_saved, failed_authors
+	author_filename = re.sub(' ', '_', author_fullname)
+	
 	# save results to .json and .csv
-	with open(f'{OUTPUT_FOLDER}/saved_articles/json/{author_fullname}.json', 'w') as aus:
+	with open(f'{OUTPUT_FOLDER}/saved_articles/json/{author_filename}.json', 'w') as aus:
 		json.dump(author_articles, aus, indent=2)
 
 	df = pd.DataFrame({
@@ -58,18 +68,18 @@ def save_results(author_fullname, author_articles):
 		'authors': [x['authors'] for x in author_articles],
 		'doi': [x['doi'] for x in author_articles],
 	})
-	df.to_csv(f'{OUTPUT_FOLDER}/saved_articles/csv/{author_fullname}.csv', index = None, encoding="utf-8")
+	df.to_csv(f'{OUTPUT_FOLDER}/saved_articles/csv/{author_filename}.csv', index = None, encoding="utf-8")
 	
 	# update list of authors already searched
-	with open('{OUTPUT_FOLDER}/authors_already_searched.json', 'w') as aus:
+	with open(f'{OUTPUT_FOLDER}/authors_already_searched.json', 'w') as aus:
 		json.dump(authors_already_searched, aus)
 	
 	# update list of articles already saved
-	with open('{OUTPUT_FOLDER}/articles_already_saved.json', 'w') as aus:
+	with open(f'{OUTPUT_FOLDER}/articles_already_saved.json', 'w') as aus:
 		json.dump(articles_already_saved, aus)
 	
 	# update list of failed authors
-	with open('{OUTPUT_FOLDER}/failed_authors.json', 'w') as aus:
+	with open(f'{OUTPUT_FOLDER}/failed_authors.json', 'w') as aus:
 		json.dump(failed_authors, aus)
 	
 
@@ -78,6 +88,7 @@ i = 0
 # for each author
 for author_fullname in authors_to_search:
 	author = authors_to_search[author_fullname]
+	author_articles = []
 	try:
 		# get search term
 		fname = author['firstname']
@@ -86,13 +97,13 @@ for author_fullname in authors_to_search:
 		MAX_RESULTS = 100000
 
 		# check if author already searched
-		if fname+' '+lname in authors_already_searched: 
-			print('***** SKIPPED', fname, lname)
+		if author_fullname in authors_already_searched: 
+			print('***** SKIPPED', author_fullname)
 			continue
-		
+	
 		# search for articles by this author
 		results = pubmed.query(SEARCH_TERM, max_results=MAX_RESULTS)
-		
+	
 		# format each article result
 		for result in results:
 			article = result.toDict()
@@ -115,7 +126,7 @@ for author_fullname in authors_to_search:
 	except:
 		print('\n\n######### FAILED #########\n')
 		print(author)
-		print('\n#########\n\n')
+		print(f'\n######### total failed: {len(failed_authors)} #########\n\n')
 		failed_authors.append(author)
 	
 	# save results
@@ -124,7 +135,7 @@ for author_fullname in authors_to_search:
 	
 	# save progress
 	i += 1
-	if not i % 500: 
+	if not i % 100:
 		print(f'{len(articles_already_saved)} articles saved, {len(authors_already_searched)} / {len(authors_to_search)} authors searched')
 	
 	time.sleep(1)
